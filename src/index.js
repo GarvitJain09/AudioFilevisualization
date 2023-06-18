05cbc16a53ae41f6aac2942c1111c65ef4a72017
Garvit Jain <garvitjain09@gmail.com>
2023-06-18 06:59:13 +0000

    Created Iniatl Upload controller, added a function to read file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+function handleFiles(event) {
+  var files = event.target.files;
+  $("#src").attr("src", URL.createObjectURL(files[0]));
+  document.getElementById("audio").load();
+  document.getElementById("audio").classList.remove("hide");
+}
+document
+  .getElementById("upload")
+  .addEventListener("change", handleFiles, false);
